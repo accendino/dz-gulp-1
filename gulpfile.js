2eea7e72abd9d2d68ab6551a3cef6ccd1bf3e0e7
accendino <achendina@gmail.com>
2018-07-30 22:35:15 +0300

    Less task, Homework to check

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,18 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
-var less = require('gulp-less');
+var styles = require('gulp-less');
 
-gulp.task('server', ['less'], function() {
+gulp.task('server', ['styles'], function() {
     browserSync.init({
     	server: { baseDir: './app/'}
     });
     gulp.watch('./app/**/*.html').on('change', browserSync.reload);
-    gulp.watch('./app/less/**/*.less', ['less']);
+    gulp.watch('./app/less/**/*.less', ['styles']);
 });
 
-gulp.task('less', function() {
+gulp.task('styles', function() {
     return gulp.src('./app/less/**/*.less')
-    .pipe(less())
+    .pipe(styles())
     .pipe(gulp.dest('./app/css'))
     .pipe(browserSync.stream());
 });
