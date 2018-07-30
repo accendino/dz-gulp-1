0416563cdc17942bb91342f2e5e596a7d54ef9a4
accendino <achendina@gmail.com>
2018-07-30 22:02:04 +0300

    Less task IS FIXED

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,13 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var less = require('gulp-less');
-var sass = require('gulp-sass');
 
 gulp.task('server', ['less'], function() {
     browserSync.init({
     	server: { baseDir: './app/'}
     });
     gulp.watch('./app/**/*.html').on('change', browserSync.reload);
-    // gulp.watch('./app/less/**/*.less', ['less']);
-    gulp.watch('./app/sass/**/*.scss', ['sass']);
+    gulp.watch('./app/less/**/*.less', ['less']);
 });
 
 gulp.task('less', function() {
